8b3f8f388155b93b7160501328a9aa36aed11651
hyeok314 <joysys8272@gmail.com>
2026-05-31 16:54:20 +0900

    JPA 기초 23강

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -13,22 +13,17 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Setter
 //@RequiredArgsConstructor
 @ToString
-// @NoArgsConstructor
+@NoArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    private final int id;
+    private int id;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public Post(String title, String content) {
-        this.id = 0;
         this.title = title;
         this.content = content;
-    }
-
-    public Post() {
-        this(null, null);
     }
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -26,9 +26,12 @@ public class BaseInitData {
     void work1() { // 생성 로직
         if(postRepository.count() > 0) return;
 
-        Post post1 = postRepository.save(new Post("제목 1", "내용 1"));
+        Post post1 = new Post("제목 1", "내용 1");
+        postRepository.save(post1);
         Post post2 = postRepository.save(new Post("제목 2", "내용 2"));
 
+        System.out.println(post1.getId());
+        System.out.println(post2.getId());
         System.out.println("기본 데이터가 초기화되었습니다.");
     }
 
