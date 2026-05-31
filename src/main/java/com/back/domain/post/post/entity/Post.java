ff9e398203d00d78cdb492d298114547ce780863
hyeok314 <joysys8272@gmail.com>
2026-05-31 16:57:39 +0900

    JPA 기초 23강

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
@@ -18,11 +20,15 @@ public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private int id;
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
     private String title;
     @Column(columnDefinition = "TEXT")
     private String content;
 
     public Post(String title, String content) {
+        this.createDate = LocalDateTime.now();
+        this.modifyDate = LocalDateTime.now();
         this.title = title;
         this.content = content;
     }
