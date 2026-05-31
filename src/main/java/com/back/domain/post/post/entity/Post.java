f331b827015a7c01fa140f1ad1a7323f0877ae02
hyeok314 <joysys8272@gmail.com>
2026-05-31 16:27:46 +0900

    JPA 기초 18강

--- src/main/java/com/back/domain/post/post/entity/Post.java
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -1,9 +1,11 @@
 package com.back.domain.post.post.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -11,10 +13,12 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Entity
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private int id;
-    private String title;
-    private String content;
+    private final String title;
+    @Column(columnDefinition = "TEXT")
+    private final String content;
 }
