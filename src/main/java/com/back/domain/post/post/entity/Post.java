85f370ebbe0babf58d96e72347570c306564d41a
hyeok314 <joysys8272@gmail.com>
2026-05-31 16:03:11 +0900

    JPA 기초 7강

---
+++ src/main/java/com/back/domain/post/post/entity/Post.java
@@ -0,0 +1,19 @@
+package com.back.domain.post.post.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+    private String title;
+}
