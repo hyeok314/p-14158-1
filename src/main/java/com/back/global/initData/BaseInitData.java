2159ba47b29dc544128318d6ed58e14a5b17b0da
hyeok314 <joysys8272@gmail.com>
2026-05-31 17:08:28 +0900

    JPA 기초 29강

--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -3,6 +3,7 @@ package com.back.global.initData;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.repository.PostRepository;
 import com.back.domain.post.post.service.PostService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +12,9 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Optional;
 
 @Configuration
+@AllArgsConstructor
 public class BaseInitData {
     private PostService postService;
-
-    @Autowired
-    public BaseInitData(PostService postService) {
-        this.postService = postService;
-    }
 
     @Bean
     ApplicationRunner baseInitDataApplicationRunner() {
